db9ea5077dee0539539121c8fd4cdd87965ea783
Jie Zhang <jiez@ucsc.edu>
2015-04-17 10:02:28 -0700

    Decide if two strings are anagrams or not.

---
+++ Ch1 ArraysAndStrings/CC1_4.java
@@ -0,0 +1,34 @@
+/*
+CC1.4: Write a method to decide if two strings are anagrams or not.
+*/
+
+import java.util.*;
+
+public class CC1_4 {
+    public static void main (String[] args){
+        String A = "apple";
+        String B = "leapp";
+        System.out.println(isAnagrams(A, B));
+    }
+    
+    public static boolean isAnagrams(String A, String B) {
+        if (A == null || B == null) {
+            return false;
+        }
+        
+        if (A.length() != B.length()) {
+            return false;
+        }
+        
+        char[] arrA = A.toCharArray();
+        char[] arrB = B.toCharArray();
+        
+        Arrays.sort(arrA);
+        Arrays.sort(arrB); // Then I also can do Arrays.equals(arrA, arrB).
+        
+        if (String.valueOf(arrA).equals(String.valueOf(arrB))) { // Do not write code like String.valueOf(arrA) == String.valueOf(arrB)
+            return true;
+        }
+        return false;
+    }
+}
